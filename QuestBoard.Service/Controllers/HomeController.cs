6557968ebc1fed83d85eed18fdcba267b28af5a0
Theun Schut <t.schut@euphoria-it.nl>
2025-06-28 22:21:46 +0200

    Added clickevent on quest card to either go to details page or to manage page based on logged in user

--- QuestBoard.Service/Controllers/HomeController.cs
+++ QuestBoard.Service/Controllers/HomeController.cs
@@ -3,13 +3,26 @@ using QuestBoard.Domain.Interfaces;
 
 namespace QuestBoard.Service.Controllers;
 
-public class HomeController(IQuestService questService) : Controller
+public class HomeController(IQuestService questService, IUserService userService) : Controller
 {
     [HttpGet]
     public async Task<IActionResult> Index(CancellationToken token = default)
     {
         var quests = await questService.GetQuestsWithSignupsAsync(token);
 
+        // Get current user if authenticated to check if they're a DM
+        string? currentUserName = null;
+        if (User.Identity?.IsAuthenticated == true)
+        {
+            var userEntity = await userService.GetUserAsync(User);
+            if (userEntity != null)
+            {
+                var user = await userService.GetByIdAsync(userEntity.Id, token);
+                currentUserName = user?.Name;
+            }
+        }
+
+        ViewBag.CurrentUserName = currentUserName;
         return View(quests);
     }
 }
