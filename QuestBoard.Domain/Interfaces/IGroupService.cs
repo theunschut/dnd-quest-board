5fe61a028ca4730e8c3b885027c0cf045b3e3d8f
Theun Schut <t.schut@euphoria-it.nl>
2026-06-30 15:27:37 +0200

    feat(29-03): add IGroupService, IGroupRepository interfaces and GroupWithMemberCount DTO
    - IGroupRepository in Domain/Interfaces extends IBaseRepository<Group> with GetAllWithMemberCountAsync, HasMembersAsync, AddMemberAsync, RemoveMemberAsync, GetMembersAsync
    - IGroupService in Domain/Interfaces extends IBaseService<Group> with same five methods
    - GroupWithMemberCount DTO in Domain/Models with Id, Name, CreatedAt, MemberCount

---
+++ QuestBoard.Domain/Interfaces/IGroupService.cs
@@ -0,0 +1,13 @@
+using QuestBoard.Domain.Enums;
+using QuestBoard.Domain.Models;
+
+namespace QuestBoard.Domain.Interfaces;
+
+public interface IGroupService : IBaseService<Group>
+{
+    Task<IList<GroupWithMemberCount>> GetAllWithMemberCountAsync(CancellationToken token = default);
+    Task<bool> HasMembersAsync(int groupId, CancellationToken token = default);
+    Task AddMemberAsync(int groupId, int userId, GroupRole groupRole, CancellationToken token = default);
+    Task RemoveMemberAsync(int groupId, int userId, CancellationToken token = default);
+    Task<IList<UserGroup>> GetMembersAsync(int groupId, CancellationToken token = default);
+}
